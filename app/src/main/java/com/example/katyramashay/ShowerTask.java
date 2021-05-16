a7ad580ad30e0bf8f305d4e40cb56d262b7d77e0
ramabalaguru <ramabgurunath@gmail.com>
2021-05-15 22:09:07 -0400

    ShowerTask java page

--- app/src/main/java/com/example/katyramashay/ShowerTask.java
+++ app/src/main/java/com/example/katyramashay/ShowerTask.java
@@ -2,6 +2,12 @@ package com.example.katyramashay;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import com.example.katyramashay.DataModelingClasses.Controller;
+import com.example.katyramashay.DataModelingClasses.Shower;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ShowerTask extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class ShowerTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shower_task);
         getSupportActionBar().hide();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Shower shower = new Shower();
+
+        CheckBox showerCompletionCheck = findViewById(R.id.showerCompletionCheck);
+        shower.setCompletion(showerCompletionCheck.isChecked());
+
+        final Controller controller = (Controller) getApplicationContext();
+        String date = new SimpleDateFormat("EEEE, MMMM dd, yyyy", Locale.US).format(new Date());
+        controller.getDay(date).addTask(shower);
     }
 }
